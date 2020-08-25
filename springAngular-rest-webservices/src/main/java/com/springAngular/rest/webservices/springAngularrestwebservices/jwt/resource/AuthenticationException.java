f9cf2d3b55933ba56798c5009630b79621c6dd47
Niket <niket1409@gmail.com>
2020-08-25 22:24:06 +0530

    deleted the unwanted folders

--- springAngular-rest-webservices/src/main/java/com/springAngular/rest/webservices/springAngularrestwebservices/jwt/resource/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package com.springAngular.rest.webservices.springAngularrestwebservices.jwt.resource;
-
-
-public class AuthenticationException extends RuntimeException {
-	public AuthenticationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
