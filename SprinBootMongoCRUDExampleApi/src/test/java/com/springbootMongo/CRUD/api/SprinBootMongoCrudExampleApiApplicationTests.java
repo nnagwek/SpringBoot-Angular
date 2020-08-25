07806ac3d2e523ba4e5169b273d7ab33a3946a20
nnagwek <43063766+nnagwek@users.noreply.github.com>
2020-08-25 18:05:29 +0530

    Spring Boot- Mongo CRUD

---
+++ SprinBootMongoCRUDExampleApi/src/test/java/com/springbootMongo/CRUD/api/SprinBootMongoCrudExampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootMongo.CRUD.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootMongoCrudExampleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
