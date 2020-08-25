7d79471ac4703481942f611a31f1876938e9371a
nnagwek <43063766+nnagwek@users.noreply.github.com>
2020-08-25 18:04:14 +0530

    Uploading Spring Boot Rest services files

---
+++ springAngular-rest-webservices/src/main/java/com/springAngular/rest/webservices/springAngularrestwebservices/jwt/resource/AuthenticationException.java
@@ -0,0 +1,8 @@
+package com.springAngular.rest.webservices.springAngularrestwebservices.jwt.resource;
+
+
+public class AuthenticationException extends RuntimeException {
+	public AuthenticationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
---
+++ springAngular-rest-webservices/src/test/java/com/springAngular/rest/webservices/springAngularrestwebservices/SpringAngularRestWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springAngular.rest.webservices.springAngularrestwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularRestWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
